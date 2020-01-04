0ab499e8fd31a1947cd7e989b3a7669419047b3d
Anusha Datar <anushadatar@gmail.com>
2020-01-04 01:21:45 -0500

    added implementation

--- cpu.rs
+++ cpu.rs
@@ -1,6 +1,5 @@
-// Creating a cpu struct that matches chip8 specs.
-
-struct cpu{
+// Create a cpu struct that matches chip8 specs.
+struct cpu {
     // 16 8-bit register array, single reserved bit.
     regi: [u8; 16],
     // Memory address.
@@ -17,3 +16,20 @@ struct cpu{
     sound_timer: u8,
     delay_timer: u8
 }
+
+// Create an impl for instantiation.
+impl cpu {
+    fn new() -> cpu {
+        cpu {
+            regi:[0;16],
+            addr: 0x200,
+            memory: [0; 4096],
+            pc: 0x200,
+            sp: 0,
+            opcode: 0,
+            sound_timer: 0,
+            delay_timer: 0
+        }
+    }
+}
+
